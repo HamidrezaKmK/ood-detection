fix Bug in logit scaling of flows

main_ood.py
@@ -238,7 +238,7 @@ def run_ood(config: dict):
         samples = model.sample(9)
         samples = torchvision.utils.make_grid(samples, nrow=3)
         wandb.log(
-            {"data/model_samples": [wandb.Image(samples, caption="model samples")]})
+            {"data/model_generated": [wandb.Image(samples, caption="model generated")]})
 
     img_array = plot_likelihood_ood_histogram(
         model,
@@ -252,8 +252,9 @@ def run_ood(config: dict):
     # (4) Instantiate an OOD solver and run #
     #########################################
     
+    # For dummy runs that you just use for visualization
     if "method_args" not in config["ood"] or "method" not in config["ood"]:
-        print("No method available! Exiting...")
+        print("No ood method available! Exiting...")
         return
     
     method_args = copy.deepcopy(config["ood"]["method_args"])
@@ -264,8 +265,8 @@ def run_ood(config: dict):
     if config["ood"]["seed"] is not None:
         np.random.seed(config["ood"]["seed"])
     idx = np.random.randint(len(out_loader))
-    for _ in range(idx):
-        x, y, _ = next(iter(out_loader))
+    for _ in range(idx + 1):
+        x, _, _ = next(iter(out_loader))
 
     if config["ood"]["pick_single"]:
         # pick a single image the selected batch

model_zoo/density_estimator/flow.py
@@ -10,6 +10,7 @@ import torch
 from pprint import pprint
 from nflows.nn import nets as nets
 import copy
+import math
 
 class RQCouplingTransform(PiecewiseRationalQuadraticCouplingTransform):
     """
@@ -71,20 +72,64 @@ class RQCouplingTransform(PiecewiseRationalQuadraticCouplingTransform):
             **kwargs,
         )
 
+class RescalingTransform(Transform):
+    """
+    This class performs elementwise rescaling.
+    This is performed via dividing every pixel or element
+    by a constant `rescaling_factor`.
+    """
+    def __init__(self, rescaling_factor: float = 256.):
+        super().__init__()
+        self.rescaling_factor = 1.0 / rescaling_factor
+    
+    def forward(self, inputs, context=None):
+        dims = list(range(1, len(inputs.shape)))
+        
+        # make an all ones vector of dimension inputs.shape[0] with the same device and dtype as inputs
+        return self.rescaling_factor * inputs, torch.sum(torch.ones_like(inputs), dim=dims) * math.log(self.rescaling_factor) 
+    
+    def inverse(self, inputs, context=None):
+        dims = list(range(1, len(inputs.shape)))
+        
+        return 1.0 / self.rescaling_factor * inputs, -torch.sum(torch.ones_like(inputs), dim=dims) * math.log(self.rescaling_factor)
+    
 class LogitTransform(Transform):
     """Base class for all transform objects."""
-    def __init__(self, eps=0.05):
+    def __init__(self, alpha=0.05, eps=1e-6):
         super().__init__()
+        
+        if alpha < eps:
+            raise ValueError("alpha must be greater than eps")
+        
+        self.alpha = alpha
         self.eps = eps
         
+    def log(self, x: torch.Tensor):
+        """Log function with a small offset for numerical stability."""
+        return torch.log(x + self.eps)
+    
+    def logit(self, x: torch.Tensor):
+        """Logit function with a small offset for numerical stability."""
+        return torch.log(x / (1 - x + self.eps))
+    
     def forward(self, inputs, context=None):
-        inputs = torch.clamp(inputs, self.eps, 1 - self.eps)
-        return torch.logit(inputs), -torch.sum(torch.log(1 - inputs) + torch.log(inputs))
-
+        # get all the non-batch dimensions
+        dims = list(range(1, len(inputs.shape)))
+        
+        pre_logit = self.alpha + (1 - self.alpha) * inputs
+        
+        logdets = torch.sum(math.log(1 - self.alpha) - self.log(1 - pre_logit) - self.log(pre_logit), dim=dims)
+        
+        
+        return self.logit(pre_logit), logdets
+        
     def inverse(self, inputs, context=None):
+        # get all the non-batch dimensions
+        dims = list(range(1, len(inputs.shape)))
+        
         sigm = torch.sigmoid(inputs)
-        sigm_clamped = torch.clamp(sigm, self.eps, 1 - self.eps)
-        return sigm, torch.sum(torch.log(1 - sigm_clamped) + torch.log(sigm_clamped))
+        
+        return sigm, torch.sum(self.log(1 - sigm) + self.log(sigm) - math.log(1 - self.alpha), dim=dims)
     
 class NormalizingFlow(DensityEstimator):
     """
@@ -193,6 +238,15 @@ class NormalizingFlow(DensityEstimator):
         if self.logit_transform:
             self.transform = CompositeTransform([LogitTransform(), self.transform])
             self.logit_transform = False
+        
+        # rescaling is part of the transforms, then include it in the flow transforms
+        if self.scale_data:
+            # TODO: the rescaling factor part is janky! Fix it!
+            self.transform = CompositeTransform([
+                RescalingTransform(rescaling_factor=255. + self.dequantize), 
+                self.transform
+            ])
+            self.scale_data = False
             
         # Setup the actual flow underneath
         self._nflow = Flow(
@@ -209,16 +263,10 @@ class NormalizingFlow(DensityEstimator):
     def log_prob(self, x):
         # NOTE: Careful with log probability when using _data_transform()
         x = self._data_transform(x)
-        # print the maximum and minimum of x
-        # print("x max: ", x.max()) 
-        # print("x min: ", x.min())
         log_prob = self._nflow.log_prob(x)
         # check if log_prob has nan values, otherwise print the maximum and minimum of log_prob
         if torch.isnan(log_prob).any():
             raise ValueError("log_prob has nan values")
-        # else:
-        #     print("log_prob max: ", log_prob.max())
-        #     print("log_prob min: ", log_prob.min())
             
         if len(log_prob.shape) == 1:
             log_prob = log_prob.unsqueeze(1)

ood/visualization.py
@@ -146,10 +146,10 @@ def visualize_trends(
     mean_plus_std_reference_scores = []
     
     for _, i in zip(t_values, range(scores.shape[1])):
-        scores = scores[:, i]
-        avg_scores = np.mean(scores)
-        upper_scores = scores[scores >= avg_scores]
-        lower_scores = scores[scores <= avg_scores]
+        scores_ = scores[:, i]
+        avg_scores = np.mean(scores_)
+        upper_scores = scores_[scores_ >= avg_scores]
+        lower_scores = scores_[scores_ <= avg_scores]
         
         mean_scores.append(avg_scores)
         mean_minus_std.append(avg_scores - np.std(lower_scores))
@@ -189,17 +189,27 @@ def visualize_trends(
             )
         })
     else:
-        ys = [mean_scores]
-        keys = [f"{y_label}-mean"]
         if reference_scores is not None:
-            ys = [mean_reference_scores] + ys
-            keys = ["ref-{y_label}-mean"] + keys
-        wandb.log({
-            f"trend/{title}": wandb.plot.line_series(
-                xs = t_values,
-                ys = ys,
-                keys = keys,
-                title = title,
-                xname = x_label,
-            )
-        })
+            ys = [mean_reference_scores, mean_scores]
+            keys = [f"ref-{y_label}-mean", f"{y_label}-mean"]
+            wandb.log({
+                f"trend/{title}": wandb.plot.line_series(
+                    xs = t_values,
+                    ys = ys,
+                    keys = keys,
+                    title = title,
+                    xname = x_label,
+                )
+            })
+        else:
+            # if no reference or no STD is given, there is no reason to
+            # plot line series, we can simply plot a line plot
+            table = wandb.Table(data = [[x, y] for x, y in zip(t_values, mean_scores)], columns = [x_label, y_label])
+            wandb.log({
+                f"trend/{title}": wandb.plot.line(
+                    table,
+                    x_label,
+                    y_label,
+                    title=title,
+                )
+            })